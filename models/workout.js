93cb01c183b631ddc80c93db1ecc85138f4e0e37
NXTGenforcer <gskywalker11@gmail.com>
2022-01-19 21:11:54 -0500

    init commit

---
+++ models/workout.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: String,
+    trim: true,
+  },
+  type: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+});
